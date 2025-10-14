b814097e8e1648d31e309967e327cee359cde3d6
aasshop100 <aasshop100@gmail.com>
2025-10-14 15:28:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -205,7 +205,7 @@ function handleCheckoutSubmit(event) {
 
     // Initialize EmailJS with your User ID
     try {
-        emailjs.init('eHXhTKYnIawMoj-Im');
+        emailjs.init('Ylc8ivuwjh3t5i7Bp');
     } catch (err) {
         console.warn('EmailJS init might have already happened:', err);
     }
@@ -293,3 +293,4 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
